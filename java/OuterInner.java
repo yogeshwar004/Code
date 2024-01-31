b34de09534c50f24317186db5d6b0d79a62515e2
Yogeshwar N <yogeshwar2004@gmail.com>
2024-01-31 14:46:25 +0530

    hghg

--- java/OuterInner.java
+++ java/OuterInner.java
@@ -1,31 +1,5 @@
-class Outer
+class A 
 {
-    private int x=10;
-    class Inner
-    {
-        private int y=20;
-        void display()
-        {
-            System.out.println("Inner Class");
-            System.out.println("X="+x+"\tY="+y);
-        }
-    }
-    void display()
-    {
-        Inner i=new Inner();
-        i.display();
-        System.out.println("Outer Class");
-        System.out.println("Y="+i.y);
-    }
-}
-public class OuterInner
-{
-    public static void main(String[] args)
-    {
-        Outer o=new Outer();
-        Outer.Inner obj=o.new Inner();
+    
 
-        o.display();
-        obj.display();
-    }
 }
