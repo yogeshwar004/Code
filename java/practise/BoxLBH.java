f8975028836e021578959df2562aee252a009edd
Yogeshwar N <yogeshwar2004@gmail.com>
2024-03-05 13:57:37 +0530

    gh

---
+++ java/practise/BoxLBH.java
@@ -0,0 +1,18 @@
+class BoxIn
+{
+    static double l,b,h;
+}
+public class BoxLBH
+{
+    public static void main(String[] args)
+    {
+        int a;
+        BoxIn obj;
+        obj=new BoxIn();
+        obj.l=10;
+        obj.b=20;
+        obj.h=30;
+        System.out.println("l="+obj.l+"\tb="+obj.b+"\th="+obj.h);
+    
+    }
+}
